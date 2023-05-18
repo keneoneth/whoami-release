1e6db5f23e3e1efdcd58ca8cf8ce20c1154c5642
Li Chun Ting <keneo@DESKTOP-0UPGAMU.localdomain>
2023-05-18 23:41:08 +0800

    Updates

---
+++ precache-manifest.d06aad6ad9a05a811f95131a93703b47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad02eb9ed910407d3ef5",
+    "url": "/whoami-release/static/css/main.543fe6d5.chunk.css"
+  },
+  {
+    "revision": "ad02eb9ed910407d3ef5",
+    "url": "/whoami-release/static/js/main.ad02eb9e.chunk.js"
+  },
+  {
+    "revision": "3e49b5171f86bdb3ad5a",
+    "url": "/whoami-release/static/js/1.3e49b517.chunk.js"
+  },
+  {
+    "revision": "28a96946df71be6e7382",
+    "url": "/whoami-release/static/js/2.28a96946.chunk.js"
+  },
+  {
+    "revision": "d84b7520204e237374d4",
+    "url": "/whoami-release/static/js/runtime~main.d84b7520.js"
+  },
+  {
+    "revision": "207485de2e9f55d5207706145725aaa1",
+    "url": "/whoami-release/index.html"
+  }
+];
